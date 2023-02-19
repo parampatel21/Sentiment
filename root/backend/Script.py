007d4cc6bbafa755ed19690ec0bc78a004e557ef
ChrisLam790 <chrislam790@gmail.com>
2023-02-19 00:25:14 -0500

    Begin donwload text script func
    Create tester code

--- root/backend/Script.py
+++ root/backend/Script.py
@@ -4,7 +4,6 @@
 from textblob import TextBlob as tb
 
 
-
 class Script:
     def __init__(self,userID,title,dateCreated,scriptContent):
         #ID metadata
@@ -26,7 +25,7 @@ class Script:
         
         
         
-    
+    ###Getters and setters
     def getUserID(self):
         return self.userID
 
@@ -43,11 +42,21 @@ class Script:
     def setPersonalNotes(self, personalNotes):
         self.personalNotes = personalNotes
         return True
-        
+    
     def setScriptContent(self, scriptContent):
         self.scriptContent = scriptContent
         return True
-    
+   
+   
+        """Create formatted text file & download to user's download folder
+        """
+    def downloadScript(self):
+       with open("~/Desktop/Script.txt", "w") as text_file:
+        text_file.write("User ID: %s\n" % self.getUserID())
+        text_file.write("Date Created: %s\n" %self.getDateCreated())
+        text_file.write("Current Script: %s\n" % self.getScriptContent())
+        
+        
     def getScriptContent(self):
         return self.scriptContent
         
@@ -56,8 +65,18 @@ class Script:
         print(5)
         text = "I had a good day"
         blob = tb(text)
-        tags = blob.sentiment
+        tags = blob.tags
         print(tags)
+
     
-        
+###Tester code for creating txt file
+def main():
+    testScript = Script(userID= "ChrisL", 
+           title= "New Script",
+           dateCreated= "02/19/2023",
+           scriptContent= "This is a text script. Hello!")
+    
+    testScript.downloadScript()
+    
+main()
     
