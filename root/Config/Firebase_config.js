bcf6febcffc38f3f5cc62df2f76506efab0cdbd8
ChrisLam790 <chrislam790@gmail.com>
2023-02-19 18:37:12 -0500

    Added json for credentials for adding data
    Updated Config for firestore

--- root/Config/Firebase_config.js
+++ root/Config/Firebase_config.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
+import exp from "constants";
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider} from "firebase/auth"
-
+import {getFirestore} from "firebase/firestore"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,3 +24,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const providerGoogle = new GoogleAuthProvider();
+export const db = getFirestore(app);
