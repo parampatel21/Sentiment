dc53bc36d38213aa94b9e8c696e49a86a5082173
ChrisLam790 <chrislam790@gmail.com>
2023-02-18 23:49:38 -0500

    Added scriptContent variable for initialization
    Added respective getters and setters
    
    Return True for all setters for confirmation of set
    
    Adjust comments

--- root/backend/Script.py
+++ root/backend/Script.py
@@ -1,27 +1,29 @@
 #Performance script class
 
-#Sentiment analysis
+#Sentiment analysis lib
 from textblob import TextBlob as tb
 
 
 
 class Script:
-    def __init__(self,userID,title,dateCreated):
+    def __init__(self,userID,title,dateCreated,scriptContent):
         #ID metadata
         self.userID = userID
         self.title = title
         self.dateCreated = dateCreated
+        self.scriptContent = scriptContent
         
-        #Emotional analysis report (For video-script comparisson)
+        ###TODO Emotional analysis report (For video-script comparisson)
         self.emotionalReport = None
         
-        #Other report
+        ###TODO Other report
         self.wordChoiceReport = None
         self.sentenceChoiceReport = None
         
-        #Combined report
+        ###TODO Combined report
         self.combinedReport = None
         self.personalNotes = None
+        
         
         
     
@@ -33,15 +35,24 @@ class Script:
     
     def setTitle(self, title):
         self.title = title
+        return True
 
     def getDateCreated(self):
         return self.dateCreated
 
     def setPersonalNotes(self, personalNotes):
         self.personalNotes = personalNotes
+        return True
+        
+    def setScriptContent(self, scriptContent):
+        self.scriptContent = scriptContent
+        return True
     
+    def getScriptContent(self):
+        return self.scriptContent
+        
+    ###TODO
     def sentimentAnalysis():
-        #TODO
         print(5)
         text = "I had a good day"
         blob = tb(text)
