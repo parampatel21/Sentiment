b8cef68b61a03b9c7fc5fac299f665d74a8f2d5d
ChrisLam790 <chrislam790@gmail.com>
2023-02-18 23:21:47 -0500

    Created class structure

--- Script.py
+++ Script.py
@@ -1,1 +1,43 @@
 #Performance script class
+import nltk
+nltk.download("text2emotion")
+import text2emotion as te
+
+
+
+class Script:
+    def __init__(self,userID,title,dateCreated):
+        #ID metadata
+        self.userID = userID
+        self.title = title
+        self.dateCreated = dateCreated
+        
+        #Emotional analysis report (For video-script comparisson)
+        self.emotionalReport = None
+        
+        #Other report
+        self.wordChoiceReport = None
+        self.sentenceChoiceReport = None
+        
+        #Combined report
+        self.combinedReport = None
+        self.personalNotes = None
+        
+        
+    
+    def getUserID(self):
+        return self.userID
+
+    def getTitle(self):
+        return self.title
+    
+    def setTitle(self, title):
+        self.title = title
+
+    def getDateCreated(self):
+        return self.dateCreated
+
+    def setPersonalNotes(self, personalNotes):
+        self.personalNotes = personalNotes
+        
+    
