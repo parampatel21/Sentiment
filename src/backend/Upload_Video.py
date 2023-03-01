0d4199486bb871bb7741edfc37354ba7b1db7ce5
ChrisLam790 <chrislam790@gmail.com>
2023-02-28 20:53:49 -0500

    testing further

--- src/backend/Upload_Video.py
+++ src/backend/Upload_Video.py
@@ -14,21 +14,9 @@ config = {
     "measurementId": "G-38C03L0RC1"
 }
 
-from google.cloud import storage
-from google.oauth2 import service_account
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
-    
-    cwd = os.path.dirname(os.path.realpath("serviceAccountKey.json"))
+firebase = pyrebase.initialize_app(config=config)
+storage = firebase.storage()
 
-    #Conect to firestore DB via seviceAccKey
-    credentials = service_account.Credentials.from_service_account_file(cwd + "/serviceAccountKey.json")    
-    firebase_admin.initialize_app(credentials)
-    
-    storage_client = storage.Client(credentials=credentials)
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-    blob.upload_from_filename(source_file_name)
-    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
-    
-    
-upload_blob(firebase_admin.storage.bucket().name, 'Script.txt', 'images/beatiful_picture.jpg')
+path_on_cloud = "Scripts/Script.txt"
+path_local = "Script.txt"
+storage.child(path_on_cloud).put(path_local)
