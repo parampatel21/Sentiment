be43778295210ad81c0cb01fc71a8022fc32084f
ChrisLam790 <chrislam790@gmail.com>
2023-03-02 18:03:05 -0500

    work on downlaod file

--- src/backend/DBWriter.py
+++ src/backend/DBWriter.py
@@ -223,7 +223,10 @@ def downloadFile(uid, index):
     try:
         bucket = storage.bucket()
         blob = bucket.blob(uid + "_" + str(index))
-        blob.download_to_filename(getVideoTitle(uid=uid, index=index) + ".mp4")
+        file_ptr = getVideoTitle(uid=uid, index=index) + ".mp4" 
+        blob.download_to_filename(file_ptr)
+        
+        os.rename(os.path.realpath(file_ptr) + "/" + str(file_ptr), os.path.realpath("Downloads") + "/" + str(file_ptr))
         return True
     except FileNotFoundError:
         #Should not be thrown iff UI implemented correctly
@@ -255,7 +258,7 @@ def deleteFile(uid, index):
         return False
 
 #print(uploadFile("uid",1, "Script.txt"))
-#print(downloadFile(uid="uid", index=1))
+print(downloadFile(uid="uid", index=1))
 
 
 
