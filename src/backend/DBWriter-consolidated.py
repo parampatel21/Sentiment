ed84096fd26ab19aac120dfddcc0c8f87ac85a9d
Param Patel <51967918+parampatel21@users.noreply.github.com>
2023-03-12 11:14:21 -0500

    thinking it through

---
+++ src/backend/DBWriter-consolidated.py
@@ -0,0 +1,5 @@
+# ISSUES:
+#   1. Need to consolidate all functions into one master function
+#   2. Need to figure out how to authenticate Firebase Storage and Firestore (either from client side in function parameter or in cloud function)
+#   3. Find a way to setup Github Actions to deploy straight to firebase functions
+#       i. probably this thing: https://github.com/marketplace/actions/cloud-functions-deploy
