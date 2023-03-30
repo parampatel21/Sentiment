2ee4d21b0af65939d07cce7b1ad08b6942d79c70
Param Patel <51967918+parampatel21@users.noreply.github.com>
2023-03-30 17:42:44 -0400

    rudimentary video upload started

--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import { useNavigate } from 'react-router-dom'
-import axios from 'axios';
+import {storage} from '../firebase'
 import Navbar from './components/Navbar';
 import '../styles/HomePage.css'
 
@@ -42,23 +42,79 @@ function ViewAllPerformances() {
         //     .catch(error => console.error(error));
     };
 
-    async function use_axios() {
-        axios({
-            method: 'post',
-            url: 'https://us-central1-sentiment-379415.cloudfunctions.net/test_function',
-            data: {
-                message: 'Hello'
-            }
+    const [recording, setRecording] = useState(false);
+    const [videoBlob, setVideoBlob] = useState(null);
+    const [videoSrc, setVideoSrc] = useState(null);
+    const [playing, setPlaying] = useState(false);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [uploaded, setUploaded] = useState(false);
+    const videoRef = useRef(null);
+  
+    const handleStartRecording = () => {
+      navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+        .then((mediaStream) => {
+          videoRef.current.srcObject = mediaStream;
+  
+          const mediaRecorder = new MediaRecorder(mediaStream);
+          const chunks = [];
+  
+          mediaRecorder.addEventListener('dataavailable', (event) => {
+            chunks.push(event.data);
+          });
+  
+          mediaRecorder.addEventListener('stop', () => {
+            const videoBlob = new Blob(chunks, { type: 'video/mp4' });
+            setVideoBlob(videoBlob);
+            setVideoSrc(URL.createObjectURL(videoBlob));
+            setRecording(false);
+          });
+  
+          mediaRecorder.start();
+          setRecording(true);
         })
-            .then((response) => {
-                console.log(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
-
-
+        .catch((error) => {
+          console.error('Error accessing media devices:', error);
+        });
+    };
+  
+    const handleStopRecording = () => {
+      videoRef.current.srcObject.getTracks().forEach((track) => track.stop());
+      videoRef.current.srcObject = null;
+    };
+  
+    const handlePlayback = () => {
+      setPlaying(true);
+      videoRef.current.play();
+    };
+  
+    const handlePause = () => {
+      setPlaying(false);
+      videoRef.current.pause();
+    };
+  
+    const handleScrub = (event) => {
+      setCurrentTime(event.target.value);
+      videoRef.current.currentTime = event.target.value;
+    };
+  
+    const handleReset = () => {
+      setVideoBlob(null);
+      setVideoSrc(null);
+      setCurrentTime(0);
+      setUploaded(false);
+    };
+  
+    const handleUpload = () => {
+      const storageRef = storage;
+      const fileName = `${Date.now()}.mp4`;
+      const videoRef = storageRef.child(`videos/${fileName}`);
+  
+      videoRef.put(videoBlob).then((snapshot) => {
+        console.log('Uploaded a blob or file!', snapshot);
+        setUploaded(true);
+      });
+    };  
+  
 
     return (
         <div className="container-fluid">
@@ -86,6 +142,61 @@ function ViewAllPerformances() {
                         </div>
                     ))}
                 </div>
+
+                {videoSrc ? (
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <video
+                    src={videoSrc}
+                    ref={videoRef}
+                    autoPlay={!playing}
+                    onTimeUpdate={() => setCurrentTime(videoRef.current.currentTime)}
+                    playsInline={true}
+                    style={{ width: '400px', height: 'auto' }}
+                    />
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <input
+                        type="range"
+                        min={0}
+                        max={videoRef.current.duration}
+                        step={0.1}
+                        value={currentTime}
+                        onChange={handleScrub}
+                        style={{ width: '400px', margin: '0 10px' }}
+                    />
+                    {playing ? (
+                        <button onClick={handlePause}>Pause</button>
+                    ) : (
+                        <>
+                        {!uploaded && (
+                            <button onClick={handleUpload}>Upload</button>
+                        )}
+                        <button onClick={handlePlayback}>Play</button>
+                        </>
+                    )}
+                    <button onClick={handleReset}>Reset</button>
+                    </div>
+                </div>
+                ) : (
+                <div>
+                    <video
+                    ref={videoRef}
+                    autoPlay={true}
+                    muted={true}
+                    playsInline={true}
+                    style={{ width: '400px', height: 'auto' }}
+                    />
+                    <div>
+                    {recording ? (
+                        <button onClick={handleStopRecording}>Stop Recording</button>
+                    ) : (
+                        <button onClick={handleStartRecording}>Start Recording</button>
+                    )}
+                    </div>
+                </div>
+                )}
+
+
+
             </main >
             <footer>
                 <p>&copy; 2023 Sentiment. All rights reserved.</p>
