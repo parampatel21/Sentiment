2d3ed03051cd0e53b31335c46c6a27140318d37b
ChrisLam790 <chrislam790@gmail.com>
2023-04-09 22:29:28 -0400

    fixed lowest to include ties

--- demos/TextAnalysisDemo.py
+++ demos/TextAnalysisDemo.py
@@ -3,13 +3,10 @@ from nrclex import NRCLex
 def analyze_text(text):
     doc = NRCLex(text)
     emotions = doc.raw_emotion_scores
-    lowest_emotions = sorted(emotions.items(), key=lambda x: x[1])[:3]
-    
     
     # create a dictionary to store the results
     results = {}
     results['raw_emotion_scores'] = emotions
-    results['lowest_emotions'] = dict(lowest_emotions)
     
     # get the lowest 3 values from my_dict1 and any ties that exceed the lowest 3
     lowest_values = [item for item in sorted(results['raw_emotion_scores'].items(), key=lambda x: x[1])[:3]]
@@ -18,21 +15,13 @@ def analyze_text(text):
     # add any ties to the lowest 3 values and convert the result to a dictionary for my_dict1
     result = {item[0]: item[1] for item in lowest_values} 
     result.update({item[0]: item[1] for item in sorted(ties)})
-    print(result)
-
-    # get the lowest 3 values from my_dict2 and any ties that exceed the lowest 3
-    lowest_values = [item for item in sorted(results['lowest_emotions'].items(), key=lambda x: x[1])[:3]]
-    ties = set([item for item in sorted(results['lowest_emotions'].items(), key=lambda x: x[1])[3:] if item[1] == lowest_values[-1][1]])
-
-    # add any ties to the lowest 3 values and convert the result to a dictionary for my_dict2
-    result = {item[0]: item[1] for item in lowest_values} 
-    result.update({item[0]: item[1] for item in sorted(ties)})
-    print(result)
     
+
+    results['lowest_emotions'] = result
     
     return results
 
-text = "love love love love hate hate hate hate"
+text = "love love love love hate hate hate"
 results = analyze_text(text)
 
 print("\n")
