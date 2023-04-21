f9e8f4f3e8dcf7bdb388ba497e93b780e4584949
Michael Jannetta <michaeljannetta3@gmail.com>
2023-04-20 23:59:34 -0400

    Working text analysis function

--- demos/jstest.js
+++ demos/jstest.js
@@ -1,20 +1,19 @@
-const spawner = require('child_process').spawn;
+function getTextAnalysis(uid, index) {
+    const spawner = require('child_process').spawn;
 
-const data_to_pass_in = {
-    data_sent: 'Send this to python script.',
-    data_returned: undefined
-};
+    const dbtest_dict = {
+        uid: uid,
+        index: index,
+        data_returned: undefined
+    };
 
-const dbtest_dict = {
-    uid: 'XrD8vDF13QQgv6HLEZz9brdo54N2',
-    index: 1,
-    data_returned: undefined
-};
+    console.log('Data sent to python script:', dbtest_dict);
 
-console.log('Data sent to python script:', dbtest_dict);
+    const python_process = spawner('python', ['./demos/TextAnalyzer.py', JSON.stringify(dbtest_dict)]);
 
-const python_process = spawner('python', ['./demos/analyzer.py', JSON.stringify(dbtest_dict)]);
-
-python_process.stdout.on('data', (data) => {
-    console.log("Data received from python script:", JSON.parse(data.toString()));
+    python_process.stdout.on('data', (data) => {
+        console.log("Data received from python script:", JSON.parse(data.toString()));
 });
+}
+//Put desired uid and index in method and it will return the full text analysis - some parsing might be necessary
+getTextAnalysis('XrD8vDF13QQgv6HLEZz9brdo54N2', 1)
